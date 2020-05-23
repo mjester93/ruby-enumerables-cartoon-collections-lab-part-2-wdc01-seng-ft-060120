3fb64e22b9b9f62d1a87a10c7050aea7c535f3f0
Michael Jester <m.jester.93@gmail.com>
2020-05-23 00:34:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,6 +28,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
 
   planeteer_calls.find do |x|
-    x.include?(valid_calls)
+    x.include? valid_calls
   end
 end
